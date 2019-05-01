30ccc7bff54e8f02b26623d4e00692fa87b13adf
Dennis <dennissarmiento808@gmail.com>
2019-04-30 20:27:44 -0700

    up

---
+++ client/build/precache-manifest.3f2f6ba7701fc19c9a87d11b1876e926.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11702e624452fd3a4393a924dd626ac4",
+    "url": "/google-books/index.html"
+  },
+  {
+    "revision": "c36eb72295ae6816e893",
+    "url": "/google-books/static/css/main.174b53f3.chunk.css"
+  },
+  {
+    "revision": "43abf99b72cb3ecff642",
+    "url": "/google-books/static/js/2.82fc1211.chunk.js"
+  },
+  {
+    "revision": "c36eb72295ae6816e893",
+    "url": "/google-books/static/js/main.b3940ef7.chunk.js"
+  },
+  {
+    "revision": "8f93ff04aa88ec00be30",
+    "url": "/google-books/static/js/runtime~main.6b0629ed.js"
+  }
+]);
